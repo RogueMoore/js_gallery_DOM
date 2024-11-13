036720e0e33b799406b16e15666c61e85c3d8fc7
Vitaly Grodetsky <rogue.moore.private@gmail.com>
2024-11-13 19:57:19 +0200

    finished using preventDefault method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const galleryList = document.querySelector('#thumbs');
+const listItems = document.querySelectorAll('.list-item');
+const imgLink = document.querySelectorAll('.list-item__link');
+
+galleryList.addEventListener('click', function (event) {
+  if (event.target.tagName === 'IMG') {
+    mainImage.src = event.target.parentElement.href;
+  }
+
+  event.preventDefault();
+});
+
+console.dir(mainImage);
